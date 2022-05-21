133c192f0b1cc0240be8ddf88852ddfaf81ccce6
Alex Lane <97alane@gmail.com>
2022-05-21 11:50:52 -0500

    setting up local storage and submit and clear buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,21 @@ var forecastDiv = $("#weatherForecast");
 var citiesArray;
 
 
+$("#submitCity").click(function () {
+    event.preventDefault();
+    let cityName = $("#cityInput").val();
+    returnCurrentWeather(cityName);
+    returnWeatherForecast(cityName);
+});
+
+if (localStorage.getItem("localWeatherSearches")) {
+    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
+    writeSearchHistory(citiesArray);
+} else {
+    citiesArray = [];
+};
+
+$("#clear").click(function () {
+    localStorage.clear('localWeatherSearches');
+});
+
