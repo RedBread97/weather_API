a20576fdaf5c6627c21aa0c6a3545263fb11bd5c
Alex Lane <97alane@gmail.com>
2022-05-21 11:57:49 -0500

    setting up functions for getting weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,20 @@ $("#clear").click(function () {
     localStorage.clear('localWeatherSearches');
 });
 
+function returnCurrentWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function (response) {
+
+        let currentTime = new Date(response.dt * 1000);
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currentWeatherDiv.html(`
+        <h2>${response.name}, ${response.sys.country} (${currentTime.getMonth() + 1}/${currentTime.getDate()}/${currentTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <p>Temperature: ${response.main.temp}&#176;F</p>
+        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Wind Speed: ${response.wind.speed} mph</p>
+        `, returnUVIndex(response.coord))
+        createHistoryButton(response.name);
+    })
+};
